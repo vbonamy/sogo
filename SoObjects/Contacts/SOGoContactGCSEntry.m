e2453c7fe9994dcd02fd1600d387e933b3a2f36d
Francis Lachapelle <flachapelle@inverse.ca>
2014-06-19 15:51:57 -0400

    JSON-based operations on contacts

--- SoObjects/Contacts/SOGoContactGCSEntry.m
+++ SoObjects/Contacts/SOGoContactGCSEntry.m
@@ -79,6 +79,11 @@
   return [self ldifRecord];
 }
 
+- (void) setAttributes: (NSDictionary *) newAttributes
+{
+  [[self vCard] setAttributes: newAttributes];
+}
+
 - (BOOL) hasPhoto
 {
   return ([[self vCard] firstChildWithTag: @"photo"] != nil);
